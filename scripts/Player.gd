c5d2ee89673cba549aa21fc398ab6b59e27da51f
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2024-01-26 19:10:46 -0300

    feat: Player moviment / Fixed camera on player #2 #4

---
+++ scripts/Player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+	
+	pass
